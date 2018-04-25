f7dbf4e68490f0640ec78d131268979e42270521
通捷创科 <280675809@qq.com>
2018-04-25 11:45:27 +0800

    更新 swagger.php

--- app/swagger.php
+++ app/swagger.php
@@ -1,28 +1,30 @@
-<?php 
- Class Controller
-
- @package App\Http\Controllers
-
- @SWG\Swagger(
-     basePath="",
-     host="127.0.0.1",
-     schemes={"http"},
-     @SWG\Info(
-         version="1.0",
-         title="OpenApi",
-         @SWG\Contact(name="Pek Ratanak", url="https://www.google.com"),
-     ),
-     @SWG\Definition(
-         definition="Error",
-         required={"code", "message"},
-         @SWG\Property(
-             property="code",
-             type="integer",
-             format="int32"
-         ),
-         @SWG\Property(
-             property="message",
-             type="string"
-         )
-     )
- )
+<?php
+/**
+ * Class Controller
+ *
+ * @package App\Http\Controllers
+ *
+ * @SWG\Swagger(
+ *     basePath="",
+ *     host="127.0.0.1",
+ *     schemes={"http"},
+ *     @SWG\Info(
+ *         version="1.0",
+ *         title="OpenApi",
+ *         @SWG\Contact(name="Pek Ratanak", url="https://www.google.com"),
+ *     ),
+ *     @SWG\Definition(
+ *         definition="Error",
+ *         required={"code", "message"},
+ *         @SWG\Property(
+ *             property="code",
+ *             type="integer",
+ *             format="int32"
+ *         ),
+ *         @SWG\Property(
+ *             property="message",
+ *             type="string"
+ *         )
+ *     )
+ * )
+ */
