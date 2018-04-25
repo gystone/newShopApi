fd67d1d4b1c7dbb2a31350de11276a47d3b850f1
通捷创科 <280675809@qq.com>
2018-04-25 11:48:23 +0800

    更新 web.php

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('test', 'TestController@index');
+
 Route::group(['prefix' => 'wechat'], function () {
     Route::any('init', 'WechatController@serve');
 
