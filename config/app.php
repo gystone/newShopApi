f3dfa63b55bddf41573a1169f809aa8de1a34cb9
通捷创科 <280675809@qq.com>
2018-04-25 11:57:29 +0800

    更新 app.php

--- config/app.php
+++ config/app.php
@@ -163,6 +163,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        \L5Swagger\L5SwaggerServiceProvider::class,
 
         /*
          * Package Service Providers...
