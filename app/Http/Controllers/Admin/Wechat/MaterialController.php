404dfcc309d7b2e00bc4cfa72f5c2b39d145f842
xiaolony <xiaolony@hotmail.com>
2018-05-14 11:46:00 +0800

    图文素材

--- app/Http/Controllers/Admin/Wechat/MaterialController.php
+++ app/Http/Controllers/Admin/Wechat/MaterialController.php
@@ -221,7 +221,7 @@ class MaterialController extends Controller
         $path = 'wechat/images/'.date('Y-m-d').'/'.md5(date('Y-m-d H:i:s'));
 
         if (Storage::disk('admin')->put($path, $image)) {
-            $res = $this->material->uploadImage('uploads/'.$path);
+            $res = $this->material->uploadImage('uploads/'.$path);Log::info($res);Log::info($path);Log::info($image);
             $path = Storage::disk('admin')->url($path);
 
             if ($res['errcode'] === 0) {
