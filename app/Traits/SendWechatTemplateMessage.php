9fc8fbf47542b4b120d48fcd5f697a3696f6b944
xiaolony <xiaolony@hotmail.com>
2018-05-14 18:05:01 +0800

    图文素材

---
+++ app/Traits/SendWechatTemplateMessage.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Traits;
+
+use EasyWeChat\OfficialAccount\Application;
+
+/**
+ * 发送模板消息
+ * Trait SendWechatTemplateMessage
+ * @package App\Traits
+ */
+trait SendWechatTemplateMessage
+{
+    protected $template_message;
+
+    public function __construct(Application $application)
+    {
+        $this->template_message = $application->template_message;
+    }
+
+    public function send($openid, $template_id, $url, array $data)
+    {
+        return $this->template_message->send([
+            'touser' => $openid,
+            'template_id' => $template_id,
+            'url' => $url,
+            'data' => $data,
+        ]);
+    }
+}
