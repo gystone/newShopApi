9247c7a68b3e7059a823e6d50618b650d3c42a88
xiaolony <xiaolony@hotmail.com>
2018-05-14 17:44:45 +0800

    图文素材

--- app/Http/Controllers/Admin/Wechat/MaterialController.php
+++ app/Http/Controllers/Admin/Wechat/MaterialController.php
@@ -217,7 +217,7 @@ class MaterialController extends Controller
 
         }
 
-        $res_news = $this->material->get($res['media_id']);
+        $res_news = $this->material->get($wechatMaterial->media_id);
         $material_content['news_item'] = $this->getNewsItem($res_news['news_item']);
         $material_content['update_time'] = date('Y-m-d H:i:s');
         $wechatMaterial->content = $material_content;
