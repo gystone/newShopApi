e592d42c2bb837acb49d52fac674b2fab9887446
xiaolony <xiaolony@hotmail.com>
2018-05-15 11:45:03 +0800

    response

--- app/Http/Controllers/Admin/Wechat/MaterialController.php
+++ app/Http/Controllers/Admin/Wechat/MaterialController.php
@@ -26,7 +26,7 @@ class MaterialController extends ApiController
      */
     public function materialSync()
     {
-        try {
+//        try {
             Log::info('正在同步图片素材');
             $offset = 0;
             $count = 20;
@@ -154,9 +154,9 @@ class MaterialController extends ApiController
 //            Log::info('音频素材同步完成');
 
             return $this->message('同步成功');
-        } catch (\Exception $exception) {
-            return $this->failed('同步失败，错误：'.$exception->getMessage());
-        }
+//        } catch (\Exception $exception) {
+//            return $this->failed('同步失败，错误：'.$exception->getMessage());
+//        }
     }
 
     /**
