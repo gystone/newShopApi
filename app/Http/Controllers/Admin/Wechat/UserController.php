7d250f573599a8ff5cbf1e1637eaa0a697361eb1
xiaolony <xiaolony@hotmail.com>
2018-05-16 14:39:15 +0800

    user

--- app/Http/Controllers/Admin/Wechat/UserController.php
+++ app/Http/Controllers/Admin/Wechat/UserController.php
@@ -37,7 +37,8 @@ class UserController extends ApiController
                         'country' => $user['country'],
                         'headiimgurl' => $user['headimgurl'], // FIXME: 修改字段名多了个i
                         'subscribe_time' => date('Y-m-d H:i:s', $user['subscribe_time']),
-                        'status' => 'subscribe'
+                        'status' => 'subscribe',
+                        'unionid' => '',
                     ]
                 );
                 Log::info('同步粉丝入库' . $user['nickname'] . $v);dd($list);
