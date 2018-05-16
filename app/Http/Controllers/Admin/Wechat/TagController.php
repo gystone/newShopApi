2b02b94d475920f320fcdf0202a3198c4a856970
xiaolony <xiaolony@hotmail.com>
2018-05-16 11:07:52 +0800

    tag

--- app/Http/Controllers/Admin/Wechat/TagController.php
+++ app/Http/Controllers/Admin/Wechat/TagController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Admin\Wechat;
 
 use App\Http\Controllers\ApiController;
+use App\Models\Wechat\WechatTag;
 use EasyWeChat\OfficialAccount\Application;
+use Illuminate\Support\Facades\Log;
 
 class TagController extends ApiController
 {
@@ -19,6 +21,23 @@ class TagController extends ApiController
     {
         $list = $this->tag->list();
 
+        try {
+            Log::info('正在同步标签');
+            foreach ($list as $k => $v) {
+                WechatTag::updateOrCreate([
+                    'id' => $v['id'],
+                    'name' => $v['name']
+                ], [
+                    'id' => $v['id'],
+                    'name' => $v['name'],
+                    'count' => $v['count']
+                ]);
+            }
+            Log::info('标签同步完成');
+        } catch (\Exception $exception) {
+            $this->failed('同步失败，请稍候重试');
+        }
 
+        return $this->message('同步完成');
     }
 }
