8d8cacf29795644ecbec65b7eb3a909cde3928a2
xiaolony <xiaolony@hotmail.com>
2018-05-17 17:48:28 +0800

    wechat reply

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -69,8 +69,8 @@ class WechatController extends Controller
                 case 'text':
                     Log::info($message['Content']);
                     $replys = WechatReply::where('is_open', 1);
-Log::info($replys->all());
-                    foreach ($replys->latest() as $reply) {Log::info($reply);
+                    $replys_all = $replys->latest();
+                    foreach ($replys_all as $reply) {Log::info($reply);
                         if ($reply->is_equal === 'equal' && $reply->keyword === $message['Content']) {
                             return $this->messageContent($reply);
                         } elseif ($reply->is_equal === 'contain' && stripos($message['Content'], $reply->keyword)) {
