57b23bf61a500193a1131597b78ec10c0aa51b42
xiaolony <xiaolony@hotmail.com>
2018-05-18 10:15:52 +0800

    wechat reply

--- app/Http/Controllers/Admin/Wechat/MenuController.php
+++ app/Http/Controllers/Admin/Wechat/MenuController.php
@@ -60,7 +60,9 @@ class MenuController extends ApiController
             ]);
             return $this->message('创建成功');
         } else {
-            return $this->setStatusCode(400)->message('创建失败，请稍候重试','error');
+
+            dd(123);
+//            return $this->setStatusCode(400)->message('创建失败，请稍候重试','error');
         }
     }
 
