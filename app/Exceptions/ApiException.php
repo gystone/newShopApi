531219238ea3f4f7e5cdcadb8835b185a448c94c
zhaozhen <270281156@qq.com>
2018-05-21 17:01:48 +0800

    Api异常 默认值

--- app/Exceptions/ApiException.php
+++ app/Exceptions/ApiException.php
@@ -4,7 +4,7 @@ namespace App\Exceptions;
 
 class ApiException extends \Exception
 {
-    public function __construct($msg = '', $code = 500)
+    public function __construct($msg = '内部错误', $code = 500)
     {
         parent::__construct($msg, $code);
     }
