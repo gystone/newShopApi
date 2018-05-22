467aa191a6e7e0226cfdec45add76ba8ab6eb013
xiaolony <xiaolony@hotmail.com>
2018-05-22 16:15:39 +0800

    admin role

--- app/Http/Resources/Admin/Role.php
+++ app/Http/Resources/Admin/Role.php
@@ -14,23 +14,23 @@ class Role extends Resource
      */
     public function toArray($request)
     {
-        $checkedCities = [];
-        foreach ($this->premissions as $premission) {
-            if (isset($premission['children'])) {
-                foreach ($premission['children'] as $child) {
-                    $checkedCities = array_merge($checkedCities, $child['meta']['checkedCities']);
-                }
-            } else {
-                $checkedCities = array_merge($checkedCities, $premission['meta']['checkedCities']);
-            }
-        }
+//        $checkedCities = [];
+//        foreach ($this->premissions as $premission) {
+//            if (isset($premission['children'])) {
+//                foreach ($premission['children'] as $child) {
+//                    $checkedCities = array_merge($checkedCities, $child['meta']['checkedCities']);
+//                }
+//            } else {
+//                $checkedCities = array_merge($checkedCities, $premission['meta']['checkedCities']);
+//            }
+//        }
         return array(
             'id' => $this->id,
             'name' => $this->name,
             'slug' => $this->slug,
 //            'permissions' => new RolePermissionCollection($this->permissions),
-//            'permissions' => $this->permissions,
-            'checkedCities' => $checkedCities,
+            'permissions' => $this->permissions,
+//            'checkedCities' => $checkedCities,
             'created_at' => $this->created_at !== null ? date_format($this->created_at, 'Y-m-d H:i:s') : null,
             'updated_at' => $this->updated_at !== null ? date_format($this->updated_at, 'Y-m-d H:i:s') : null,
         );
