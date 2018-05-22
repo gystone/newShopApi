979c8b34897ad57b27bb104f42c30e1fbda958f5
xiaolony <xiaolony@hotmail.com>
2018-05-22 17:55:10 +0800

    admin role

--- app/Http/Resources/Admin/Role.php
+++ app/Http/Resources/Admin/Role.php
@@ -14,26 +14,13 @@ class Role extends Resource
      */
     public function toArray($request)
     {
-        $checkedCities = [];
-        if (is_array($this->permissions)) {
-            foreach ($this->permissions as $permission) {
-                if (isset($permission['children'])) {
-                    foreach ($permission['children'] as $child) {
-                        $checkedCities = array_merge($checkedCities, $child['meta']['checkedCities']);
-                    }
-                } elseif (isset($permission['meta'])) {
-                    $checkedCities = array_merge($checkedCities, $permission['meta']['checkedCities']);
-                }
-            }
-        }
-
         return array(
             'id' => $this->id,
             'name' => $this->name,
             'slug' => $this->slug,
 //            'permissions' => new RolePermissionCollection($this->permissions),
-            'permissions' => $this->getCheckedCities(),
-            'checkedCities' => $checkedCities,
+            'permissions' => $this->permissions,
+            'checkedCities' => $this->getCheckCities(),
             'created_at' => $this->created_at !== null ? date_format($this->created_at, 'Y-m-d H:i:s') : null,
             'updated_at' => $this->updated_at !== null ? date_format($this->updated_at, 'Y-m-d H:i:s') : null,
         );
--- app/Traits/CheckedCities.php
+++ app/Traits/CheckedCities.php
@@ -6,6 +6,18 @@ trait CheckedCities
 {
     public function getCheckedCities()
     {
-        dd($this);
+        $checkedCities = [];
+        if (is_array($this->permissions)) {
+            foreach ($this->permissions as $permission) {
+                if (isset($permission['children'])) {
+                    foreach ($permission['children'] as $child) {
+                        $checkedCities = array_merge($checkedCities, $child['meta']['checkedCities']);
+                    }
+                } elseif (isset($permission['meta'])) {
+                    $checkedCities = array_merge($checkedCities, $permission['meta']['checkedCities']);
+                }
+            }
+        }
+        return $checkedCities;
     }
 }
