978c73d655b8517fb57ffb49f0067cfeed8e92b9
xiaolony <xiaolony@hotmail.com>
2018-05-22 18:02:54 +0800

    admin role

--- app/Http/Resources/Admin/Role.php
+++ app/Http/Resources/Admin/Role.php
@@ -20,7 +20,7 @@ class Role extends Resource
             'slug' => $this->slug,
 //            'permissions' => new RolePermissionCollection($this->permissions),
             'permissions' => $this->permissions,
-            'checkedCities' => $this->getCheckCities(),
+            'checkedCities' => $this->getCheckedCities(),
             'created_at' => $this->created_at !== null ? date_format($this->created_at, 'Y-m-d H:i:s') : null,
             'updated_at' => $this->updated_at !== null ? date_format($this->updated_at, 'Y-m-d H:i:s') : null,
         );
