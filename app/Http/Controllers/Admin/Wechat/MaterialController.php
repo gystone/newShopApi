a6883c4c7777bdf5b10d9cf4aa9c5254958e2b60
xiaolony <xiaolony@hotmail.com>
2018-05-22 12:19:37 +0800

    admin role

--- app/Http/Controllers/Admin/Wechat/MaterialController.php
+++ app/Http/Controllers/Admin/Wechat/MaterialController.php
@@ -103,7 +103,7 @@ class MaterialController extends ApiController
 
             foreach ($video_list['item'] as $k => $v) {
                 $video_source = $this->material->get($v['media_id']);
-                $path = 'wechat/videos/'.array_pop(explode('/', $video_source['title']));
+                $path = 'wechat/videos/'.parse_url($video_source['title'])['path']['basename'];dd($path);
                 if (Storage::disk('admin')->put($path, file_get_contents($video_source['down_url']))) {
                     $video_path = Storage::disk('admin')->url($path);
                     WechatMaterial::updateOrCreate([
