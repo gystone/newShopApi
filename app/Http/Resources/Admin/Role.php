354eaabe3d6ee44fc625770e411630e92421db21
xiaolony <xiaolony@hotmail.com>
2018-05-22 16:25:31 +0800

    admin role

--- app/Http/Resources/Admin/Role.php
+++ app/Http/Resources/Admin/Role.php
@@ -16,7 +16,7 @@ class Role extends Resource
     {
         $checkedCities = [];
         if (is_array($this->permissions)) {
-            foreach ($this->permissions as $permission) {dd($permission);
+            foreach ($this->permissions as $permission) {
                 if (isset($permission['children'])) {
                     foreach ($permission['children'] as $child) {
                         $checkedCities = array_merge($checkedCities, $child['meta']['checkedCities']);
