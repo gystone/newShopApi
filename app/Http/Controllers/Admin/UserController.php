c40a34915339704cad747dcd6d442c27dfe91159
xiaolony <xiaolony@hotmail.com>
2018-05-23 16:35:56 +0800

    admin role

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -37,7 +37,7 @@ class UserController extends ApiController
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(UserRequest $request)
     {
         DB::beginTransaction();
 
