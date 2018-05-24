ff40e30c2cba81d26ab3f24ef72ec1dd3cbc8ef6
xiaolony <xiaolony@hotmail.com>
2018-05-24 15:24:36 +0800

    fix tagusers

--- app/Http/Resources/Admin/RoleCollection.php
+++ app/Http/Resources/Admin/RoleCollection.php
@@ -14,6 +14,11 @@ class RoleCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return $this->collection;
+        return [
+            'data' => $this->collection,
+            'total' => $this->total(),
+            'per_page' => $this->perPage(),
+            'current_page' => $this->currentPage(),
+        ];
     }
 }
