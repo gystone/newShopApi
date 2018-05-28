46598fc943de49f378fb252891c541433a3174ac
xiaolony <xiaolony@hotmail.com>
2018-05-28 12:17:25 +0800

    wechat users

--- app/Http/Resources/Admin/User.php
+++ app/Http/Resources/Admin/User.php
@@ -19,9 +19,10 @@ class User extends Resource
             'id' => $this->id,
             'username' => $this->username,
             'name' => $this->name,
-            'avatar' => Storage::disk('admin')->url($this->avatar ?? 'default.jpg'),
-            'avatar_path' => $this->avatar,
-            'roles' =>  $this->roles->first() ? $this->roles->first()->name : null, //new RoleUserCollection($this->roles),
+            'avatar_url' => Storage::disk('admin')->url($this->avatar ?? 'default.jpg'),
+            'avatar' => $this->avatar,
+            'roles_name' =>  $this->roles->first() ? $this->roles->first()->name : null,
+            'roles' => new RoleUserCollection($this->roles),
             'created_at' => $this->created_at !== null ? date_format($this->created_at, 'Y-m-d H:i:s') : null,
             'updated_at' => $this->updated_at !== null ? date_format($this->updated_at, 'Y-m-d H:i:s') : null,
         );
