7d992e8efe57b0d5196e4fab3b532a8660ac3b56
xiaolony <xiaolony@hotmail.com>
2018-06-07 17:02:46 +0800

    wechat users search

--- app/Http/Controllers/Admin/Wechat/BroadcastRecordController.php
+++ app/Http/Controllers/Admin/Wechat/BroadcastRecordController.php
@@ -40,58 +40,17 @@ class BroadcastRecordController extends ApiController
                 'is_cron' => $request->is_cron,
                 'send_time' => $send_time,
             ]);
-
-//            if ($record) {
-//                return $this->success($record);
-//            } else {
-//                return $this->failed('保存失败, 未存入数据库');
-//            }
         } else {
             $send_time = date('Y-m-d H:i');
             $delay = 0;
 
-            // 群发消息
-//            switch ($request->types) {
-//                case 'text':
-//                    $message = new Text($request->message);
-//                    break;
-//                case 'image':
-//                    $message = new Image($request->message);
-//                    break;
-//                case 'news':
-//                    $message = new Media($request->message, 'mpnews');
-//                    break;
-//                case 'voice':
-//                    $message = new Media($request->message, 'voice');
-//                    break;
-//                case 'video':
-//                    $message = new Media($request->message, 'mpvideo');
-//                    break;
-//                default:
-//                    $message = new Text($request->message);
-//            }
-//
-//            $res = $this->broadcasting->sendMessage($message, $request->to['users']);
-//            if ($res['errcode'] === 0) {
-                $record = \App\Models\Wechat\BroadcastRecord::create([
-                    'tos' => $request->to,
-                    'message' => $request->message,
-                    'types' => $request->types,
-                    'is_cron' => $request->is_cron,
-                    'send_time' => $send_time
-                ]);
-//
-//                $record->msg_id = $res['msg_id'];
-//                $record->save();
-//
-//                if ($record) {
-//                    return $this->success($record);
-//                } else {
-//                    return $this->failed('发送失败, 未存入数据库');
-//                }
-//            } else {
-//                return $this->failed('发送失败, 消息未发送');
-//            }
+            $record = \App\Models\Wechat\BroadcastRecord::create([
+                'tos' => $request->to,
+                'message' => $request->message,
+                'types' => $request->types,
+                'is_cron' => $request->is_cron,
+                'send_time' => $send_time
+            ]);
 
         }
 
