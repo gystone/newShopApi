8bda916e0d9156977ca0fdf4d8f9eea35da3644c
xiaolony <xiaolony@hotmail.com>
2018-06-12 16:24:43 +0800

    update database migrate

--- database/seeds/AdminTablesSeeder.php
+++ database/seeds/AdminTablesSeeder.php
@@ -1,10 +1,8 @@
 <?php
 
-use Encore\Admin\Auth\Database\Administrator;
-use Encore\Admin\Auth\Database\Menu;
-use Encore\Admin\Auth\Database\Permission;
-use Encore\Admin\Auth\Database\Role;
 use Illuminate\Database\Seeder;
+use App\Models\Admin\AdminUser;
+use App\Models\Admin\Role;
 
 class AdminTablesSeeder extends Seeder
 {
@@ -15,23 +13,19 @@ class AdminTablesSeeder extends Seeder
      */
     public function run()
     {
-        // create a user.
-        Administrator::truncate();
-        Administrator::create([
+        AdminUser::create([
             'username' => 'admin',
             'password' => bcrypt('admin'),
             'name'     => '超级管理员',
         ]);
 
-        // create a role.
-        Role::truncate();
         Role::create([
             'name' => '超级管理员',
             'permissions' => array('admin'),
         ]);
 
         // add role to user.
-        Administrator::first()->roles()->save(Role::first());
+        AdminUser::first()->roles()->save(Role::first());
 
     }
 }
