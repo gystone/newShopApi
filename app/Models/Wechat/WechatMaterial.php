fe018808a350356aae1e5d965d08da1d0fe55a5f
XiaolongYang <xiaolony@hotmail.com>
2018-06-22 15:08:44 +0800

    更新 WechatMaterial.php

--- app/Models/Wechat/WechatMaterial.php
+++ app/Models/Wechat/WechatMaterial.php
@@ -7,7 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class WechatMaterial extends Model
 {
     protected $table = 'wechat_materials';
-    public $timestamps = false;
 
     protected $fillable = [
         'media_id', 'type', 'content'
