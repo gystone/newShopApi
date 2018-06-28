2d0010383b0a35990932e710058afd50bf3e16d4
xiaolony <xiaolony@hotmail.com>
2018-06-28 14:55:09 +0800

    微信填充

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $this->call(AdminTablesSeeder::class);
+         $this->call([
+             AdminTablesSeeder::class,
+             WechatTablesSeeder::class,
+         ]);
     }
 }
